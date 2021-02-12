320b2de328ca2359c4de39fc1cbe2c1d7cbc0ad3
xiaohuihui <tangchaohuitop@outlook.com>
2021-02-12 22:09:05 +0800

    记事本 添加关闭时提醒文件未保存 需要重写父类 直接打开记事本写入数据，保存时可以调用另存为

--- main.py
+++ main.py
@@ -15,7 +15,17 @@ class GetWin(QMainWindow, Ui_MainWindow):
         self.actionsave.triggered.connect(self.save_file)
         self.actionsave_as.triggered.connect(self.save_file_as)
 
-
+    # 重写父类 关闭
+    def closeEvent(self, e) -> None:
+        if self.textEdit.document().isModified():
+            answer = QMessageBox.question(self, "请检查文件", "文件未保存,请确认", QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
+            if answer & QMessageBox.Save:
+                self.save_file()
+            elif answer & QMessageBox.Cancel:
+                # e.accept() # 确定退出
+                e.ignore()  # 继续执行
+        else:
+            return
 
 
     def open_file(self):
@@ -23,22 +33,23 @@ class GetWin(QMainWindow, Ui_MainWindow):
         print("open")
         path = QFileDialog.getOpenFileName(self, "请选择文件")[0]  # 返回的是一个元组数据，第一个成员是文件名
         print(path)
-        if path is not None:
+        if path:
             # QMessageBox.information(self, "提示", path)
             # self.textEdit.setText(open(path,mode='rb+').read().decode("utf-8"))
             with open(path, mode='rb+') as f:
                 self.textEdit.setText(f.read().decode("utf-8"))
         else:
-            # QMessageBox.information(self, "提示", "未正确选择文件")
+            QMessageBox.information(self, "提示", "未正确选择文件")
             pass
 
     def save_file(self):
         global path
-        if path is not None:
+        if path:
             with open(path, mode='wb') as f:
                 f.write(self.textEdit.toPlainText().encode("utf-8"))
             self.textEdit.document().setModified(False)  # 文件修改标志清除
-
+        else:
+            self.save_file_as()
 
     def save_file_as(self):
         global path
