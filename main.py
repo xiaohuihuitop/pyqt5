4c3984387ba98229abecb9c1f3b0120bf9c67564
xiaohuihui <tangchaohuitop@outlook.com>
2021-02-16 22:05:05 +0800

    开始制作串口工具
    串口扫描与打开

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
-from PyQt5.QtCore import QSettings
+from PyQt5.QtCore import QSettings, QIODevice  # 配置文件使用
+from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo  # 使用qt提供的串口工具 serial 这个模块是python的
 from ui.win import Ui_MainWindow
 
 num = 0
@@ -11,6 +12,7 @@ class GetWin(QMainWindow, Ui_MainWindow):
         super(GetWin, self).__init__()
         self.setupUi(self)
 
+        # 配置文件
         self.settings = QSettings("config.ini", QSettings.IniFormat)
 
         # 获取
@@ -20,18 +22,52 @@ class GetWin(QMainWindow, Ui_MainWindow):
         # 设置
         if self.baud is None:
             self.baud = 9600
-            self.settings.setValue("SETUP/BAUD_VALUE", self.baud) # 保存
+            self.settings.setValue("SETUP/BAUD_VALUE", self.baud)  # 保存
 
         # 赋值
         self.comboBox_baud.setCurrentText(self.baud)
 
         # 信号槽
         self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
+        self.pushButton.clicked.connect(self.button_refresh_cb)
+        self.pushButton_open.clicked.connect(self.button_open_cb)
+        self.pushButton_close.clicked.connect(self.button_close_cb)
 
     def combox_baud_cb(self):
         self.baud = self.comboBox_baud.currentText()
         self.settings.setValue("SETUP/BAUD_VALUE", self.baud)  # 保存
 
+    def button_refresh_cb(self):
+        com_list = QSerialPortInfo.availablePorts()
+        for com in com_list:
+            print(com.portName())  # 返回串口号，如COM1
+            print(com.description())  # 返回设备硬件描述 如USB-SERIAL CH340
+            print(com.productIdentifier())  # 返回设备编号 如29987
+            print(com.standardBaudRates())  # 返回设备的支持波特率列表 如[110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 128000, 256000]
+            if com.portName() is not None:
+                pass
+                if self.comboBox_com.findText(com.portName()) != 1:
+                    self.comboBox_com.addItem(com.portName())
+
+
+    def button_open_cb(self):
+        print("open")
+        port = self.comboBox_com.currentText()
+        print(port)
+        com = QSerialPort()
+        com.setPortName(port)
+        if com.open(QSerialPort.ReadWrite) == False: # QSerialPort  QIODevice
+            print("open err")
+        pass
+
+    def button_close_cb(self):
+        print("close")
+        port = self.comboBox_com.currentText()
+        print(port)
+        com = QSerialPort()
+        com.setPortName(port)
+        com.close()
+        pass
 
 ui_app = QApplication([])
 main_win = GetWin()
