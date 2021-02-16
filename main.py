4fcc1346f2cccfa51685d57584c5bd23b1e011db
xiaohuihui <tangchaohuitop@outlook.com>
2021-02-16 12:53:06 +0800

    开始制作串口工具
    基础界面 配置文件读取与保存

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox, QFileDialog
+from PyQt5.QtCore import QSettings
 from ui.win import Ui_MainWindow
 
 num = 0
@@ -10,54 +11,26 @@ class GetWin(QMainWindow, Ui_MainWindow):
         super(GetWin, self).__init__()
         self.setupUi(self)
 
-        # 链接槽
-        self.actionopen.triggered.connect(self.open_file)
-        self.actionsave.triggered.connect(self.save_file)
-        self.actionsave_as.triggered.connect(self.save_file_as)
+        self.settings = QSettings("config.ini", QSettings.IniFormat)
 
-    # 重写父类 关闭
-    def closeEvent(self, e) -> None:
-        if self.textEdit.document().isModified():
-            answer = QMessageBox.question(self, "请检查文件", "文件未保存,请确认", QMessageBox.Save | QMessageBox.Discard | QMessageBox.Cancel)
-            if answer & QMessageBox.Save:
-                self.save_file()
-            elif answer & QMessageBox.Cancel:
-                # e.accept() # 确定退出
-                e.ignore()  # 继续执行
-        else:
-            return
+        # 获取
+        self.com = self.settings.value("SETUP/COM_VALUE")
+        self.baud = self.settings.value("SETUP/BAUD_VALUE")
 
+        # 设置
+        if self.baud is None:
+            self.baud = 9600
+            self.settings.setValue("SETUP/BAUD_VALUE", self.baud) # 保存
 
-    def open_file(self):
-        global path
-        print("open")
-        path = QFileDialog.getOpenFileName(self, "请选择文件")[0]  # 返回的是一个元组数据，第一个成员是文件名
-        print(path)
-        if path:
-            # QMessageBox.information(self, "提示", path)
-            # self.textEdit.setText(open(path,mode='rb+').read().decode("utf-8"))
-            with open(path, mode='rb+') as f:
-                self.textEdit.setText(f.read().decode("utf-8"))
-        else:
-            QMessageBox.information(self, "提示", "未正确选择文件")
-            pass
+        # 赋值
+        self.comboBox_baud.setCurrentText(self.baud)
 
-    def save_file(self):
-        global path
-        if path:
-            with open(path, mode='wb') as f:
-                f.write(self.textEdit.toPlainText().encode("utf-8"))
-            self.textEdit.document().setModified(False)  # 文件修改标志清除
-        else:
-            self.save_file_as()
+        # 信号槽
+        self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
 
-    def save_file_as(self):
-        global path
-        file = QFileDialog.getSaveFileName(self, "另存为", path)[0]
-        if file is not None:
-            path = file
-            self.save_file()
-
+    def combox_baud_cb(self):
+        self.baud = self.comboBox_baud.currentText()
+        self.settings.setValue("SETUP/BAUD_VALUE", self.baud)  # 保存
 
 
 ui_app = QApplication([])
