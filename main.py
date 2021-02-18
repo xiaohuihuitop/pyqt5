2ee61a6993a00f62b0e3adcc3803097a66292af4
xiaohuihui <tangchaohuitop@outlook.com>
2021-02-18 23:23:17 +0800

    开始制作串口工具
    串口发送功能测试正常
    接收功能大致完成
    但是无法触发信号 只能手动点击 清除按键 进行测试

--- main.py
+++ main.py
@@ -27,47 +27,90 @@ class GetWin(QMainWindow, Ui_MainWindow):
         # 赋值
         self.comboBox_baud.setCurrentText(self.baud)
 
+        # 串口类
+        self.com = QSerialPort()
+
         # 信号槽
         self.comboBox_baud.currentIndexChanged.connect(self.combox_baud_cb)
-        self.pushButton.clicked.connect(self.button_refresh_cb)
-        self.pushButton_open.clicked.connect(self.button_open_cb)
-        self.pushButton_close.clicked.connect(self.button_close_cb)
+        self.pushButton.clicked.connect(self.button_refresh_cb)   # 刷新端口
+        self.pushButton_open.clicked.connect(self.button_open_cb)  # 打开端口
+        self.pushButton_close.clicked.connect(self.button_close_cb)  # 关闭端口
+
+        self.pushButton_send.clicked.connect(self.button_send_cb)  # 发送数据
+        self.com.readyRead.connect(self.com_receive_cb)  # 接收数据
+        self.pushButton_clean.clicked.connect(self.button_clean_cb)  # 清除按钮
+
 
     def combox_baud_cb(self):
         self.baud = self.comboBox_baud.currentText()
         self.settings.setValue("SETUP/BAUD_VALUE", self.baud)  # 保存
 
     def button_refresh_cb(self):
-        com_list = QSerialPortInfo.availablePorts()
+        com_list = QSerialPortInfo.availablePorts()  # 获取所有的端口信息
         for com in com_list:
             print(com.portName())  # 返回串口号，如COM1
             print(com.description())  # 返回设备硬件描述 如USB-SERIAL CH340
             print(com.productIdentifier())  # 返回设备编号 如29987
             print(com.standardBaudRates())  # 返回设备的支持波特率列表 如[110, 300, 600, 1200, 2400, 4800, 9600, 14400, 19200, 38400, 56000, 57600, 115200, 128000, 256000]
-            if com.portName() is not None:
+            if com.portName() is not None:  # 判断是否为空
                 pass
-                if self.comboBox_com.findText(com.portName()) != 1:
-                    self.comboBox_com.addItem(com.portName())
+                if self.comboBox_com.findText(com.portName()) != 1:  # 判断com口是否重复
+                    self.comboBox_com.addItem(com.portName())  # 添加item
 
 
     def button_open_cb(self):
         print("open")
-        port = self.comboBox_com.currentText()
+        port = self.comboBox_com.currentText()  # 获取 COM名字
         print(port)
-        com = QSerialPort()
-        com.setPortName(port)
-        if com.open(QSerialPort.ReadWrite) == False: # QSerialPort  QIODevice
+        self.com = QSerialPort()
+        self.com.setPortName(port)  # 使用名字 绑定COM口
+        if self.com.open(QSerialPort.ReadWrite) == False:  # QSerialPort  QIODevice  打开端口
             print("open err")
+            QMessageBox.critical(self, '严重错误', '串口打开失败')
+            return
+
+        if self.com.isOpen():
+            print("is open")
+            print(self.baud)
+            self.com.setBaudRate(int(self.baud))  # 波特率
+            self.com.setDataBits(self.com.Data8)  # 8
+            self.com.setParity(self.com.NoParity)  # n
+            self.com.setStopBits(self.com.OneStop)  # 1
+            self.com.setFlowControl(self.com.NoFlowControl)
+            print("set over")
+            print(self.com.baudRate())  # 验证波特率
+
+            self.com.setDataTerminalReady(True)
         pass
 
     def button_close_cb(self):
         print("close")
         port = self.comboBox_com.currentText()
         print(port)
-        com = QSerialPort()
-        com.setPortName(port)
-        com.close()
+        self.com = QSerialPort()
+        self.com.setPortName(port)
+        self.com.close()
         pass
+
+    def button_send_cb(self):
+        print("send")
+        txData = self.plainTextEdit_Send.toPlainText()
+        print(txData)
+        self.com.write(txData.encode('UTF-8'))
+
+    def com_receive_cb(self):
+        print("receive_cb")
+        rxData = bytes(self.com.readAll())
+        if len(rxData) > 0:
+            print(rxData)
+            self.plainTextEdit_Receive.insertPlainText(rxData.decode('UTF-8'))
+
+    def button_clean_cb(self):
+        print("clean")
+        rxData = bytes(self.com.readAll())
+        if len(rxData) > 0:
+            print(rxData)
+            self.plainTextEdit_Receive.insertPlainText(rxData.decode('UTF-8'))
 
 ui_app = QApplication([])
 main_win = GetWin()
