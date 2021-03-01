1d7cbc17eff9dccd2aa736fd7becaed0ed813e3a
xiaohuihui <tangchaohuitop@outlook.com>
2021-03-01 23:39:41 +0800

    开始学习graph
    添加graph相关ui与文件

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from PyQt5.QtWidgets import QApplication
 import serial
+import graph
 
+
+# import pyqtgraph.examples
+# pyqtgraph.examples.run()
 
 ui_app = QApplication([])
-main_win = serial.GetWin()
+# main_win = serial.GetWin()
+main_win = graph.GraphWin()
 main_win.show()
+
 ui_app.exec_()
